18a0cacf95c5882486b7226b9fca449d2af408c3
Steffi <Steffi.Lauryn.Baptiste@USherbrooke.ca>
2024-11-19 20:36:27 -0500

    made a few styling changes

--- javascript/questions.js
+++ javascript/questions.js
@@ -6,7 +6,7 @@ function getQueryParam (name){
 const eventId = getQueryParam('event_id');
 const eventName = getQueryParam('event_titre');
 
-const eventInfoElement = document.getElementById('event-info');
+const eventInfoElement = document.getElementById('event-titre');
 
 if (eventId && eventName) {
     eventInfoElement.style = `text-align:center;`
@@ -48,3 +48,50 @@ fetch(apiURLQuestionsPerEvent)
   .catch(error=>{
     console.error('Error fetching questions: ', error);
   });
+
+
+const apiURLEventDetails = `https://x8ki-letl-twmt.n7.xano.io/api:25UbIoB1/event/${eventId}`;
+fetch(apiURLEventDetails)
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+})
+.then (data=>{
+    console.log(data);
+    const section = document.getElementById('eventInformation');
+    section.querySelector('#eventDescription').textContent=data.description;
+    
+    const timestampStart = data.date_heure_debut;
+    const timestampEnd = data.date_heure_fin;
+
+    const dateStart = new Date(timestampStart);
+    const dateEnd = new Date(timestampEnd);
+    const options = {
+        year: 'numeric',    // Full numeric year
+        month: 'long',      // Full month name (e.g., "novembre")
+        day: 'numeric',     // Day of the month
+        hour: '2-digit',    // Two-digit hour
+        minute: '2-digit',  // Two-digit minute
+    };
+
+    const formattedStart = new Intl.DateTimeFormat('fr-CA', options).format(dateStart);
+    const formattedEnd = new Intl.DateTimeFormat('fr-CA', options).format(dateEnd);
+
+    section.querySelector('#eventDate').innerHTML = `Début: ${formattedStart} <br> Fin: ${formattedEnd}`;
+
+    if(data.client){
+        const clientLogoURL = data.client.logo.url;
+        
+         // Set client logo
+         const clientLogoElement = section.querySelector('#eventImg');
+         if (clientLogoElement) {
+             clientLogoElement.src = clientLogoURL;
+         }
+    }
+
+})
+.catch(error=>{
+    console.error('Error fetching questions: ', error);
+});
