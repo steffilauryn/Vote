c5bd0d380f6ad4009035318f52805b9e22fda1c6
Steffi <Steffi.Lauryn.Baptiste@USherbrooke.ca>
2024-12-08 16:31:28 -0500

    fixed paths and added to server.js

--- server.js
+++ server.js
@@ -16,6 +16,51 @@ app.get('/user', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/vote-code.html'));
 });
 
+//
+//
+//
+//
+//
+// Route for calendar.html in views folder
+app.get('/calendar', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/calendar.html'));
+});
+
+// Route for create.html in views folder
+app.get('/create', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/create.html'));
+});
+
+// Route for liste-event.html in views folder
+app.get('/liste-event', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/liste-event.html'));
+});
+
+// Route for questions.html in views folder
+app.get('/questions', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/questions.html'));
+});
+
+// Route for ressource.html in views folder
+app.get('/ressource', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/ressource.html'));
+});
+
+// Route for voteur-attente.html in views folder
+app.get('/voteur-attente', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/voteur-attente.html'));
+});
+
+// Route for voteur-attente.html in views folder
+app.get('/voteur-event', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/voteur-event.html'));
+});
+
+// Route for voteur-attente.html in views folder
+app.get('/voteur-question', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/voteur-question.html'));
+});
+
 // Redirect root URL to /user
 app.get('/', (req, res) => {
     res.redirect('/user');
